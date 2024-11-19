5bb1a26b77de508194128d3540d483b41b3a97c8
Nyrew <lukas.vokel@gmail.com>
2024-11-19 23:14:27 +0100

    Authentication with JWTs

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
 	platform       string
+	jwtSecret      string
 }
 
 func main() {
@@ -40,6 +41,11 @@ func main() {
 		log.Fatal("PLATFORM must be set")
 	}
 
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is not set")
+	}
+
 	// Open database connection
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
@@ -55,6 +61,7 @@ func main() {
 		fileserverHits: atomic.Int32{},
 		db:             dbQueries,
 		platform:       platform,
+		jwtSecret:      jwtSecret,
 	}
 
 	mux := http.NewServeMux()
