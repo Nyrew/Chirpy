a17b08d17db25c458849fb96d983309cf0bfda1b
Nyrew <lukas.vokel@gmail.com>
2024-11-19 10:14:10 +0100

    feat: add request metrics and reset endpoints

--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", apiCfg.middlewareMetrianocsInc(http.StripPrefix("/app", fileServer)))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -19,4 +30,18 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println("Error starting the server:", err)
 	}
+}
+
+func (cfg *apiConfig) middlewareMetrianocsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1) // Increment the counter
+		next.ServeHTTP(w, r)      // Call the next handler
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load() // Safely read the counter
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", count)))
 }
